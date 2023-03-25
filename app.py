0d8d2e5c8139fd9a5e0a389efe9291e83446c9b8
“Justin <justinmonstot@gmail.com>
2023-03-25 14:56:11 -0700

    added css stuff.

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", debug=True) # tell it to run locally
