353f85e3f98f0955df4bbc9b9f96daef59e88526
“Justin <justinmonstot@gmail.com>
2023-03-26 15:31:13 -0700

    added login form funcationality

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
+
+# ...
 
 @app.route('/')
 @app.route('/index')
@@ -21,3 +24,8 @@ def index():
         }
     ]
     return render_template('index.html', title='MyDataConsulting', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
