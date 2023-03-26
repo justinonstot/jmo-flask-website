3da4608edc0eb1413877d248e3e733f01ac27cb9
“Justin <justinmonstot@gmail.com>
2023-03-26 15:00:51 -0700

    finished chapter 2

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,23 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    # return render_template('home.html') 
-    return "Hello World!"
+    user = {'username':'Justin'}
+    posts = [
+        {
+            'author': {'username':'Justin'},
+            'body': 'Beautiful day Portland!'
+        },
+        {
+            'author': {'username':'Susan'},
+            'body': 'The Avengers movie was so cool!' 
+        },
+        {
+            'author': {'username':'Smitty'},
+            'body': 'The sun will come out tomorrow!'
+        }
+    ]
+    return render_template('index.html', title='MyDataConsulting', user=user, posts=posts)
